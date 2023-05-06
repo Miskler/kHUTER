e8cab0cf266085d6bd10e2f77acbd88d1d88115c
Miskler <lysenkomiha913@gmail.com>
2023-05-06 23:47:14 +0300

    refactoring the file system
    in order not to break the project, the "Scenes" folder is left unchanged
    
    a gradual transition is expected with future commits

--- Script/Interfes/StoriScriptMenu.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-onready var pupaanim = $"../../sett"
-onready var Au = $"../../AudioStreamPlayer2D2"
-
-func _pressed():
-	Au.play()
-	pupaanim.play_backwards("otladca")
--- Script/Item/set_visible_area.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-onready var g = get_node("/root/editmap")
-
-func _ready():
-	if g:
-		return
-	else:
-		$ReferenceRect.editor_only = true
